e9c848d9265c9a1be1c89cc15ce7079d781d759b
MWBKris <kris@my-websitebuilder.com>
2013-09-19 23:15:49 +0200

    js

--- www/js/index.js
+++ www/js/index.js
@@ -22,7 +22,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
-		alert("start1.0");
+		alert("start1.1");
         this.bindEvents();
     },
     // Bind Event Listeners
