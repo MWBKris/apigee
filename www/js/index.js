01ecb4551e6611288940d7b19a458e34023ac789
MWBKris <kris@my-websitebuilder.com>
2013-09-19 22:21:12 +0200

    cordova2

--- www/js/index.js
+++ www/js/index.js
@@ -22,7 +22,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
-		alert("start1.1");
+		alert("start1.2");
         this.bindEvents();
     },
     // Bind Event Listeners
